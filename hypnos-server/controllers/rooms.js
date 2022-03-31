89da4a4da801f1b6a60a0b3b864574617faadd60
Joshua Vayer <77019747+joshuaVayer@users.noreply.github.com>
2022-03-31 19:32:15 +0200

    Feature : BK4 - auth, rooms, medias (#2)

---
+++ hypnos-server/controllers/rooms.js
@@ -0,0 +1,39 @@
+const Room = require("@models/rooms");
+const { generateCrudMethods } = require("@utils/mongo");
+const { isAllowedFacilityUser } = require("@utils/mongo/user");
+
+module.exports = {
+  ...generateCrudMethods(Room),
+
+  getAll: async (req, res) => {
+    const { facilityId } = req.query;
+    if (facilityId) {
+      Room.find({ facility: facilityId })
+        .then((result) => res.json(result))
+        .catch((err) => res.json(err));
+    } else {
+      return generateCrudMethods(Room).getAll(req, res);
+    }
+  },
+
+  create: async (req, res) => {
+    const isAllowed = await isAllowedFacilityUser(req, true);
+    if (!isAllowed) return res.status(403).send("Unauthorized operation or no corresponding room");
+
+    return generateCrudMethods(Room).create(req, res);
+  },
+
+  update: async (req, res) => {
+    const isAllowed = await isAllowedFacilityUser(req, true);
+    if (!isAllowed) return res.status(403).send("Unauthorized operation or no corresponding room");
+
+    return generateCrudMethods(Room).update(req, res);
+  },
+
+  remove: async (req, res) => {
+    const isAllowed = await isAllowedFacilityUser(req, true);
+    if (!isAllowed) return res.status(401).json({ message: "Unauthorized operation or no corresponding room" });
+
+    return generateCrudMethods(Room).remove(req, res);
+  }
+};
--- hypnos-server/routes/index.js
+++ hypnos-server/routes/index.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 routes.use("/auth", require("@api/auth"));
 routes.use("/users", require("@api/users"));
+routes.use("/upload", require("@api/upload"));
 routes.use("/facilities", require("@api/facilities"));
+routes.use("/rooms", require("@api/rooms"));
 
 module.exports = routes;
